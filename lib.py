90e510e54701abae5c8d06f28b744e5807090510
Carrier Pigeon <132307322+carrierpigeondev@users.noreply.github.com>
2024-06-08 00:19:16 -0500

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,7 +1,17 @@
 import time
 
-def xprint(text: str, char_delay: float = 0.02, newline: bool = True):
+PUNCT_1: list = [","]
+PUNCT_1_MULT = 10
+PUNCT_2: list = [".", "!", "?"]
+PUNCT_2_MULT = 30
+
+def xprint(text: str, char_delay: float = 0.03, newline: bool = True):
     for char in text:
         print(char, end="")
-        time.sleep(char_delay)
+        if char in PUNCT_1:
+            time.sleep(char_delay * PUNCT_1_MULT)
+        elif char in PUNCT_2:
+            time.sleep(char_delay * PUNCT_2_MULT)
+        else:
+            time.sleep(char_delay)
     if newline: print("\n")
